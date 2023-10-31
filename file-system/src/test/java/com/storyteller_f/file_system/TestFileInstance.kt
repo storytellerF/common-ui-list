d931acb29edd67bb47ad41142f7dc0cddca992ce
storytellerF <34095089+storytellerF@users.noreply.github.com>
2023-10-31 18:31:46 +0800

    添加robolectric，增加emulated 路径测试

---
+++ file-system/src/test/java/com/storyteller_f/file_system/TestFileInstance.kt
@@ -0,0 +1,23 @@
+package com.storyteller_f.file_system
+
+import android.net.Uri
+import android.os.Build
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+class TestFileInstance {
+    @Config(minSdk = Build.VERSION_CODES.JELLY_BEAN)
+    @Test
+    fun test() {
+        val parse = Uri.parse("file:///storage/emulated")
+        val context = RuntimeEnvironment.getApplication()
+        runBlocking {
+            assert(FileInstanceFactory.getLocalFileInstance(context, parse).list().count > 0)
+        }
+    }
+}
