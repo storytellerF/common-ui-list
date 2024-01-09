60afe0def41936c01ca3e2205bd87d711288a23e
storytellerF <34095089+storytellerF@users.noreply.github.com>
2024-01-09 19:29:21 +0800

    修复fileIcon 中漏掉文件夹的问题

--- file-system-ktx/src/main/java/com/storyteller_f/file_system_ktx/Common.kt
+++ file-system-ktx/src/main/java/com/storyteller_f/file_system_ktx/Common.kt
@@ -9,33 +9,37 @@ val FileInfo.isFile
 
 val FileInfo.isDirectory get() = kind.isDirectory
 
-fun ImageView.fileIcon(fileSystemItemModel: FileInfo) {
-    if (fileSystemItemModel.fullPath.startsWith("/data/app/")) {
-        setImageDrawable(context.packageManager.getApplicationIcon(fileSystemItemModel.name))
-        return
-    }
-    val extension = fileSystemItemModel.extension
-    if (extension.isEmpty()) {
-        val placeholder = when (extension) {
-            "mp3", "wav", "flac" -> R.drawable.ic_music
-            "zip", "7z", "rar" -> R.drawable.ic_archive
-            "jpg", "jpeg", "png", "gif" -> R.drawable.ic_image
-            "mp4", "rmvb", "ts", "avi", "mkv", "3gp", "mov", "flv", "m4s" -> R.drawable.ic_video
-            "url" -> R.drawable.ic_url
-            "txt", "c" -> R.drawable.ic_text
-            "js" -> R.drawable.ic_js
-            "pdf" -> R.drawable.ic_pdf
-            "doc", "docx" -> R.drawable.ic_word
-            "xls", "xlsx" -> R.drawable.ic_excel
-            "ppt", "pptx" -> R.drawable.ic_ppt
-            "iso" -> R.drawable.ic_disk
-            "exe", "msi" -> R.drawable.ic_exe
-            "psd" -> R.drawable.ic_psd
-            "torrent" -> R.drawable.ic_torrent
-            else -> R.drawable.ic_unknow
+fun ImageView.fileIcon(fileInfo: FileInfo) {
+    if (fileInfo.isFile) {
+        if (fileInfo.fullPath.startsWith("/data/app/")) {
+            setImageDrawable(context.packageManager.getApplicationIcon(fileInfo.name))
+            return
         }
-        setImageResource(placeholder)
+        val extension = fileInfo.extension
+        if (extension.isEmpty()) {
+            val placeholder = when (extension) {
+                "mp3", "wav", "flac" -> R.drawable.ic_music
+                "zip", "7z", "rar" -> R.drawable.ic_archive
+                "jpg", "jpeg", "png", "gif" -> R.drawable.ic_image
+                "mp4", "rmvb", "ts", "avi", "mkv", "3gp", "mov", "flv", "m4s" -> R.drawable.ic_video
+                "url" -> R.drawable.ic_url
+                "txt", "c" -> R.drawable.ic_text
+                "js" -> R.drawable.ic_js
+                "pdf" -> R.drawable.ic_pdf
+                "doc", "docx" -> R.drawable.ic_word
+                "xls", "xlsx" -> R.drawable.ic_excel
+                "ppt", "pptx" -> R.drawable.ic_ppt
+                "iso" -> R.drawable.ic_disk
+                "exe", "msi" -> R.drawable.ic_exe
+                "psd" -> R.drawable.ic_psd
+                "torrent" -> R.drawable.ic_torrent
+                else -> R.drawable.ic_unknow
+            }
+            setImageResource(placeholder)
+        } else {
+            setImageResource(R.drawable.ic_binary)
+        }
     } else {
-        setImageResource(R.drawable.ic_binary)
+        setImageResource(R.drawable.ic_folder_explorer)
     }
 }
