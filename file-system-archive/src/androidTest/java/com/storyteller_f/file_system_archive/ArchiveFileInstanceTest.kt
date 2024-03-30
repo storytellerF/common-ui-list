062fc15b22691f52bb9dd43039e6a479bda93a59
storytellerF <34095089+storytellerF@users.noreply.github.com>
2024-03-30 15:39:29 +0800

    增加ArchiveFileInstance嵌套的测试

--- file-system-archive/src/androidTest/java/com/storyteller_f/file_system_archive/ArchiveFileInstanceTest.kt
+++ file-system-archive/src/androidTest/java/com/storyteller_f/file_system_archive/ArchiveFileInstanceTest.kt
@@ -8,11 +8,15 @@ import com.storyteller_f.file_system.ensureFile
 import com.storyteller_f.file_system.getFileInstance
 import com.storyteller_f.file_system.instance.FileCreatePolicy
 import com.storyteller_f.file_system.toChildEfficiently
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.File
+import java.io.InputStream
+import java.io.OutputStream
 import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
 
@@ -24,10 +28,9 @@ class ArchiveFileInstanceTest {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
 
         runBlocking {
-            val file = File(appContext.filesDir, "test.zip").ensureFile() ?: return@runBlocking
+            val file = File(appContext.filesDir, "test.zip").ensureFile()!!
             ZipOutputStream(file.outputStream()).use {
-                val zipEntry = ZipEntry("hello.txt")
-                it.putNextEntry(zipEntry)
+                it.putNextEntry(ZipEntry("hello.txt"))
                 it.write("hello".toByteArray())
                 it.closeEntry()
             }
@@ -53,22 +56,64 @@ class ArchiveFileInstanceTest {
     }
 
     @Test
-    fun testLocalArchiveFileInstanceToChild() {
+    fun testLocalArchiveFileInstanceToChildEfficiently() {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
 
         runBlocking {
-            val file = File(appContext.filesDir, "test.zip").ensureFile() ?: return@runBlocking
-            ZipOutputStream(file.outputStream()).use {
-                val zipEntry = ZipEntry("hello.txt")
-                it.putNextEntry(zipEntry)
+            val zipFile = File(appContext.filesDir, "test.zip").ensureFile()!!
+            ZipOutputStream(zipFile.outputStream()).use {
+                it.putNextEntry(ZipEntry("hello.txt"))
                 it.write("hello".toByteArray())
                 it.closeEntry()
             }
 
-            val fileInstance = getFileInstance(appContext, Uri.fromFile(file))!!
+            val fileInstance = getFileInstance(appContext, Uri.fromFile(zipFile))!!
             val instance = fileInstance.toChildEfficiently(appContext, "hello.txt")
             assertEquals("hello.txt", instance.name)
         }
 
+    }
+
+    @Test
+    fun testNestedArchiveFileInstance() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+
+        runBlocking {
+            val zipFile = File(appContext.filesDir, "test.zip").ensureFile()!!
+            ZipOutputStream(zipFile.outputStream()).use {
+                it.putNextEntry(ZipEntry("hello.txt"))
+                it.write("hello".toByteArray())
+                it.closeEntry()
+            }
+
+            val parentZip = File(appContext.filesDir, "parent.zip").ensureFile()!!
+            ZipOutputStream(parentZip.outputStream()).use {
+                val zipEntry = ZipEntry("test.zip")
+                it.putNextEntry(zipEntry)
+                zipFile.inputStream().use { input ->
+                    writeStream(input, it)
+                }
+            }
+            val parentFileInstance = getFileInstance(appContext, Uri.fromFile(parentZip))!!
+            val instance = parentFileInstance.toChildEfficiently(appContext, "test.zip")
+            assertEquals("test.zip", instance.name)
+            val textFileInstance = instance.toChildEfficiently(appContext, "hello.txt")
+            assertEquals("hello.txt", textFileInstance.name)
+        }
+
+    }
+}
+
+suspend fun writeStream(inputStream: InputStream, outputStream: OutputStream) {
+    val array = ByteArray(DEFAULT_BUFFER_SIZE)
+    withContext(Dispatchers.IO) {
+        while (true) {
+            val count = inputStream.read(array)
+            if (count >= 0) {
+                outputStream.write(array, 0, count)
+            } else {
+                break
+            }
+        }
     }
 }
