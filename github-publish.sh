87e523f16d2591bdd757d06107e47abf0d4f6708
storytellerF <34095089+storytellerF@users.noreply.github.com>
2026-03-16 22:19:17 +0800

    rename group to com.storyteller_f.common_ui_list

--- github-publish.sh
+++ github-publish.sh
@@ -5,6 +5,5 @@ GPR_KEY=$(grep gpr.key local.properties | cut -d'=' -f2)
 echo "GPR_USER: $GPR_USER"
 echo "GPR_KEY: $GPR_KEY"
 export VERSION=1.0.0-SNAPSHOT
-export GROUP=com.storytellerF.common_ui_list
 
-./gradlew publishAllPublicationsToGitHubPackagesRepository -Pgpr.user="$GPR_USER" -Pgpr.key="$GPR_KEY" -Pversion=$VERSION -Pgroup="${GROUP}"
+./gradlew publishAllPublicationsToGitHubPackagesRepository -Pgpr.user="$GPR_USER" -Pgpr.key="$GPR_KEY" -Pversion=$VERSION
