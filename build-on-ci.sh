020a8d7bfe74e62aff69703aab305bedccbb1e6b
AgentStart1 <2499194949@qq.com>
2026-08-31 11:17:34 +0800

    Publish libraries to Maven Central (#2)
    * feat: publish libraries to Maven Central
    
    * ci: split test and publish workflows
    
    * ci: split app build and test workflows
    
    * ci: use default Gradle cache provider
    
    * ci: use preinstalled Android SDK
    
    * ci: update action versions
    
    * fix: use valid Maven Central publish task

--- build-on-ci.sh
+++ build-on-ci.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-mkdir -p ~/.gradle
-echo "gpr.user=$ORG_GRADLE_PROJECT_gpr_user" >> ~/.gradle/gradle.properties
-echo "gpr.key=$ORG_GRADLE_PROJECT_gpr_key" >> ~/.gradle/gradle.properties
-
 ./gradlew build
--- common-publish/src/main/kotlin/android-common-publish.gradle.kts
+++ common-publish/src/main/kotlin/android-common-publish.gradle.kts
@@ -1,26 +1,35 @@
 plugins {
-    `maven-publish`
+    id("com.vanniktech.maven.publish")
 }
 
 println("group: $group, version: $version")
 
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            afterEvaluate {
-                from(components["release"])
+mavenPublishing {
+    publishToMavenCentral(automaticRelease = true)
+    signAllPublications()
+    coordinates(artifactId = project.name)
+
+    pom {
+        name.set(project.name)
+        description.set("A module from common-ui-list, a collection of reusable Android UI and Kotlin utilities")
+        url.set("https://github.com/storytellerF/common-ui-list")
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://opensource.org/licenses/MIT")
             }
         }
-    }
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/storytellerF/common-ui-list")
-            // 最好通过命令行传递
-            credentials {
-                username = project.findProperty("gpr.user") as String
-                password = project.findProperty("gpr.key") as String
+        developers {
+            developer {
+                id.set("storytellerF")
+                name.set("storytellerF")
+                url.set("https://github.com/storytellerF")
             }
+        }
+        scm {
+            connection.set("scm:git:git://github.com/storytellerF/common-ui-list.git")
+            developerConnection.set("scm:git:ssh://github.com/storytellerF/common-ui-list.git")
+            url.set("https://github.com/storytellerF/common-ui-list")
         }
     }
 }
